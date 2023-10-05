a33b2620ec4759e84b1c6d7fa2df7189e86eeb74
Sigurd Holsen <sigurd.holsen@cognite.com>
2023-10-05 15:19:35 +0200

    lock stdout

--- src/main.rs
+++ src/main.rs
@@ -12,32 +12,36 @@ struct Args {
 }
 
 #[inline]
-fn print_duration(duration: &Duration) {
+fn print_duration(f: &mut std::io::StdoutLock, duration: &Duration) {
     let hours = duration.as_secs() / 3600;
     let minutes = (duration.as_secs() / 60) % 60;
     let seconds = duration.as_secs() % 60;
     let millis = (duration.as_millis() % 1000) / 100;
-    print!("\r{:02}:{:02}:{:02}.{}", hours, minutes, seconds, millis);
+    write!(f, "\r{:02}:{:02}:{:02}.{}", hours, minutes, seconds, millis).unwrap();
 }
-
 
 fn main() {
     let args = Args::parse();
     let start = Instant::now();
+    let mut stdout = std::io::stdout().lock();
     loop {
         let dt = Instant::now() - start;
         if dt.as_secs_f64() > args.seconds {
             break;
         }
         let remaining = Duration::from_secs_f64(args.seconds) - dt;
-        print_duration(&remaining);
-        std::io::stdout().flush().unwrap();
+        print_duration(&mut stdout, &remaining);
+        stdout.flush().unwrap();
 
         let max_sleep_seconds: f64 = 0.1;
         let sleep_time = Duration::from_secs_f64(max_sleep_seconds.min(remaining.as_secs_f64()));
         std::thread::sleep(sleep_time);
     }
-    print_duration(&Duration::from_secs(0));
-    println!(" \x1b[3mDone counting down from {:.2} seconds\x1b[0m", args.seconds);
-    std::io::stdout().flush().unwrap();
+    print_duration(&mut stdout, &Duration::from_secs(0));
+    writeln!(
+        &mut stdout,
+        " \x1b[3mDone counting down from {:.2} seconds\x1b[0m",
+        args.seconds
+    )
+    .unwrap();
 }
