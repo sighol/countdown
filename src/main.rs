73fbb41b3524755f8364151fc4a53754f2fe8a79
Sigurd Holsen <sigurd.holsen@cognite.com>
2023-10-05 14:04:14 +0200

    Wait for 100ms between iterations

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,8 @@ fn print_duration(duration: &Duration) {
     let hours = duration.as_secs() / 3600;
     let minutes = (duration.as_secs() / 60) % 60;
     let seconds = duration.as_secs() % 60;
-    let millis = (duration.as_millis() % 1000) / 10 * 10;
-    print!("\r{:02}:{:02}:{:02}.{:03}", hours, minutes, seconds, millis);
+    let millis = (duration.as_millis() % 1000) / 100;
+    print!("\r{:02}:{:02}:{:02}.{}", hours, minutes, seconds, millis);
 }
 
 
@@ -33,7 +33,7 @@ fn main() {
         print_duration(&remaining);
         std::io::stdout().flush().unwrap();
 
-        let max_sleep_seconds: f64 = 0.08;
+        let max_sleep_seconds: f64 = 0.1;
         let sleep_time = Duration::from_secs_f64(max_sleep_seconds.min(remaining.as_secs_f64()));
         std::thread::sleep(sleep_time);
     }
