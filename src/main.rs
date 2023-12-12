0b1c192be572f62a7a121f55845c1b4f9125941f
Sigurd Holsen <sigurd.holsen@cognite.com>
2023-12-12 14:55:18 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,32 @@ struct Args {
     seconds: f64,
 }
 
+fn countdown(seconds: f64) -> Result<(), Box<dyn std::error::Error>> {
+    let start = Instant::now();
+    let mut stdout = std::io::stdout().lock();
+    loop {
+        let dt = Instant::now() - start;
+        if dt.as_secs_f64() > seconds {
+            break;
+        }
+        let remaining = Duration::from_secs_f64(seconds) - dt;
+        print_duration(&mut stdout, &remaining);
+        stdout.flush()?;
+
+        let max_sleep_seconds: f64 = 0.1;
+        let sleep_time = Duration::from_secs_f64(max_sleep_seconds.min(remaining.as_secs_f64()));
+        std::thread::sleep(sleep_time);
+    }
+    print_duration(&mut stdout, &Duration::from_secs(0));
+    writeln!(
+        &mut stdout,
+        " \x1b[3mDone counting down from {:.2} seconds\x1b[0m",
+        seconds
+    )?;
+
+    Ok(())
+}
+
 #[inline]
 fn print_duration(f: &mut std::io::StdoutLock, duration: &Duration) {
     let hours = duration.as_secs() / 3600;
@@ -22,26 +48,5 @@ fn print_duration(f: &mut std::io::StdoutLock, duration: &Duration) {
 
 fn main() {
     let args = Args::parse();
-    let start = Instant::now();
-    let mut stdout = std::io::stdout().lock();
-    loop {
-        let dt = Instant::now() - start;
-        if dt.as_secs_f64() > args.seconds {
-            break;
-        }
-        let remaining = Duration::from_secs_f64(args.seconds) - dt;
-        print_duration(&mut stdout, &remaining);
-        stdout.flush().unwrap();
-
-        let max_sleep_seconds: f64 = 0.1;
-        let sleep_time = Duration::from_secs_f64(max_sleep_seconds.min(remaining.as_secs_f64()));
-        std::thread::sleep(sleep_time);
-    }
-    print_duration(&mut stdout, &Duration::from_secs(0));
-    writeln!(
-        &mut stdout,
-        " \x1b[3mDone counting down from {:.2} seconds\x1b[0m",
-        args.seconds
-    )
-    .unwrap();
+    countdown(args.seconds).unwrap();
 }
